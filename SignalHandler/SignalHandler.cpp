3d7aa5915f8f9aaed36db85e469194ef30b08b72
Khanh Nguyen <khanh@krypton.com>
2014-07-24 11:03:08 -0700

    Added some todos

--- SignalHandler/SignalHandler.cpp
+++ SignalHandler/SignalHandler.cpp
@@ -11,10 +11,15 @@
 SignalHandler* SignalHandler::mInstance = NULL;
 
 SignalHandler::SignalHandler()
-{ }
+{
+// TODO: add sig action from linux
+}
 
 SignalHandler::~SignalHandler()
-{ }
+{
+// TODO: remove sigset when signal handler is destructed
+//		 which I don't think it will.
+}
 
 SignalHandler* SignalHandler::getInstance()
 {
