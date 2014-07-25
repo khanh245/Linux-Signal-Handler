db20b0df25aa6f74a2a528fea0101bd1dd32e8e9
Khanh Nguyen <khanh@krypton.com>
2014-07-25 10:55:52 -0700

    Removed comments

--- SignalHandler/SignalHandler.cpp
+++ SignalHandler/SignalHandler.cpp
@@ -14,10 +14,6 @@ SignalHandler::SignalHandler()
 
 SignalHandler::~SignalHandler()
 {
-	/* This is for unlocking mask not destruct it
-	if (sigprocmask(SIG_SETMASK, &mMask, NULL) < 0)
-		std::cerr << "removing mask error" << std::endl;
-	*/
 	sigemptyset(&mMask);
 }
 
